cbe258ca699a244bfa39556f39cbac1e87995a57
Cowen M. Hames <63200050+Greens-Theorem@users.noreply.github.com>
2021-06-25 19:51:14 -0500

    Delete AZBarChart.py

--- AZBarChart.py
+++
@@ -1,8 +0,0 @@
-#@author Cowen M. Hames
-import seaborn as sns
-import pandas as pd
-from matplotlib import pyplot as plt
-
-df = pd.read_excel("Arizona Spreadsheet.xlsx")
-sns.barplot(data = df, x = "Med Age", y = "County")
-plt.show()
