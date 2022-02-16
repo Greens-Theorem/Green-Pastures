beb4a5f6c8ffa8eeda63e0a4669e6d2bf5a57fd4
Cowen M. Hames M.S <63200050+Greens-Theorem@users.noreply.github.com>
2022-02-16 11:41:29 -0600

    Add files via upload

---
+++ csvcore.py
@@ -0,0 +1,12 @@
+#Author C.M.H. Feb 2022 Core Pandas Data Analytics XLSX
+import pandas as pd
+import matplotlib.pyplot as plt
+#Analytics
+df = pd.read_excel(r'C:\Users\cowen\Desktop\VSC\Book1.XLSX')
+df.head()
+x = list(df['Country'])
+y = list(df['Population'])
+figure = plt.figure(figsize =(10,10))
+plt.pie(y, labels = x,autopct='%1.2f%%')
+plt.title('Countries by Population')
+plt.show()
