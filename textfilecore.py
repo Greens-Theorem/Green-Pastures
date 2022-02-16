fc70a009353565061921296ee9cc03d877f385e7
Cowen M. Hames M.S <63200050+Greens-Theorem@users.noreply.github.com>
2022-02-16 17:16:56 -0600

    Update textfilecore.py

--- textfilecore.py
+++ textfilecore.py
@@ -1,3 +1,17 @@
+#Author C.M.H. Feb 2022 Reading/Writing/Appending Text Files
+
+#Read
 file = open(r'C:\Users\cowen\Desktop\VSC\yeah.txt', 'r')
-data = file.read()
-print(data)
+data = file.readlines()
+
+#Write
+with open('yeah2.txt', 'w') as file2:
+    for line in data:
+        file2.write(line)
+        file2.write('Isnt this fun?')
+        file2.write('\n')
+
+#Append
+with open('yeah2.text', 'a') as file2:
+    oops = ['forgot this']
+    file2.writelines('\n'.join(oops))
